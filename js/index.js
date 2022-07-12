f54ad6caf98671abd6940ab583f55efaffc38901
jesse <itsjessef@gmail.com>
2022-07-12 16:00:37 -0700

    add MVC architecture

--- js/index.js
+++ js/index.js
@@ -1,140 +1,121 @@
 'use strict';
 
-let state = {'1': {
-                quantity: 1,
-                unit: 'cups',
-                ingredient: 'flour'
-              }
-            };
-let id = 0;
-let currentYieldElem = document.querySelector('#current__yield');
-let desiredYieldElem = document.querySelector('#expected__yield');
-let conversionFactor = 1;
+// Model
 
-// window.onscroll = function() {showPageLogo()};
-//
-// function showPageLogo() {
-//   let logo = document.querySelector('.page__logo');
-//   if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-//     logo.style.display = "block";
-//   } else if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//     logo.style.display = "none";
-//   } else {
-//     logo.style.display = "block";
-//   }
-// }
 
-computeConversionFactor();
-function computeConversionFactor() {
-  console.log(currentYieldElem);
-  currentYieldElem.onchange = handleXChange;
-  console.log(desiredYieldElem);
-  desiredYieldElem.onchange = handleYChange;
+let recipeInfo;
+
+recipeInfo = {
+  conversionFactor: '',
+  prevId: 0,
+  recipe: []
 }
 
-function handleXChange(e) {
-  let currentYield = e.target.value;
-  let desiredYield = desiredYieldElem.value;
-  if (desiredYield.length !== 0 && currentYield.length !== 0) {
-    conversionFactor = desiredYield / currentYield;
-    document.querySelector('.yield__factor').textContent = Math.round(conversionFactor * 100) /100;
-    console.log(conversionFactor);
+
+// Creates a new id
+const createId = () => {
+  if (recipeInfo.hasOwnProperty('prevId')) {
+    recipeInfo.prevId = recipeInfo.prevId + 1;
+  } else {
+    recipeInfo.prevId = 0;
+  }
+  return recipeInfo.prevId;
+}
+
+
+// Creates a conversion factor
+const createConversionFactor = (initialYield, expectedYield) => {
+  if (initialYield.length !== 0 && expectedYield.length !== 0) {
+    let conversionFactor = expectedYield / initialYield;
+    conversionFactor = Math.round(conversionFactor * 100) / 100;
+
+    recipeInfo.conversionFactor = conversionFactor;
   }
 }
 
-function handleYChange(e) {
-  let desiredYield = e.target.value;
-  let currentYield = currentYieldElem.value;
-  if (currentYield.length !== 0 && desiredYield.length !== 0) {
-    conversionFactor = desiredYield / currentYield;
-    document.querySelector('.yield__factor').textContent = Math.round(conversionFactor * 100) /100;
-    console.log(conversionFactor);
+// Deletes current conversion factor
+const removeConversionFactor = () => {
+  delete recipeInfo.conversionFactor;
+}
+
+// Creates a recipe item
+const createRecipeItem = (quantity, unit, ingredient) => {
+  if (ingredient.length != 0) {
+    const id = createId();
+
+    recipeInfo.recipe.push(
+      {
+        id: id,
+        quantity: quantity,
+        unit: unit,
+        ingredient: ingredient
+      }
+    );
   }
 }
 
-function makeId() {
-  id += 1;
-  return id;
-}
-
-function updateState(quantity, unit, ingredient, id) {
-  state['' + id] = {
-    quantity: quantity,
-    unit: unit,
-    ingredient: ingredient
-  };
-}
-
-addIngredient();
-function addIngredient() {
-  let addBtn = document.querySelector('.add__ingredient');
-  addBtn.addEventListener('click', function() {
-    console.log('clicked add');
-    let ingInput = document.querySelector('.recipe__add__form #ingredient').value;
-    if (ingInput.length !== 0) {
-      let qtyInput = document.querySelector('.recipe__add__form #qty').value;
-      if (qtyInput.length === 0) {
-        qtyInput = 0;
-      }
-      let unitInput = document.querySelector('.recipe__add__form #unit').value;
-      if (unitInput.length === 0) {
-        unitInput = "";
-      }
-      let currId = makeId();
-      updateState(qtyInput, unitInput, ingInput, currId);
-      let recipeItem = makeIngredient(qtyInput, unitInput, ingInput, currId);
-      let recipeContainer = document.querySelector('.recipe__add');
-      recipeContainer.appendChild(recipeItem);
-
-      let adjRecipeItem = makeAdjustedIngredient(qtyInput * conversionFactor, unitInput, ingInput, currId);
-      let adjRecipeContainer = document.querySelector('.recipe__outcome');
-      adjRecipeContainer.appendChild(adjRecipeItem);
-      includeRemoveOption(currId);
+// Deletes recipe item
+const removeRecipeItem = idToDelete => {
+  recipeInfo.recipe = recipeInfo.recipe.filter(item => {
+    if (item.id == idToDelete) {
+      return false;
+    } else {
+      return true;
     }
-  });
+  })
 }
 
-function includeRemoveOption(id) {
-  let allRemoveButtons = document.querySelectorAll('.remove__ingredient');
-  let removeButton = allRemoveButtons[allRemoveButtons.length - 1];
-  removeButton.addEventListener('click', function() {
-    removeButton.parentElement.remove();
-    let removeId = '#r' + id;
-    let correspondingIngredient = document.querySelector(removeId);
-    correspondingIngredient.remove();
-  });
+// Create adjusted recipe
+const createRecipeAdjustment = () => {
+  if (recipeInfo.hasOwnProperty('conversionFactor') && typeof recipeInfo.conversionFactor === 'number') {
+    recipeInfo.recipe.forEach(function (item) {
+      const adjustedQuantity = item.quantity * recipeInfo.conversionFactor;
+      item.adjustedQuantity = Math.round(adjustedQuantity * 1000) / 1000;
+    });
+  }
 }
 
-//console.log(makeIngredient(4, "grams", "sugar", 123));
-function makeIngredient(quantity, unit, ingredient, id) {
-  let container = document.createElement('div');
+// Saves the state
+const saveTodos = () => {
+  localStorage.setItem('recipeInfo', JSON.stringify(recipeInfo));
+}
+
+
+// View
+
+
+// generates an element for one ingredient
+const makeIngredientElem = (quantity, unit, ingredient, id) => {
+  const container = document.createElement('div');
   container.setAttribute('id', "" + id);
   container.classList.add('current__ingredient');
 
-  let qtyElem = document.createElement('input');
+  const qtyElem = document.createElement('input');
   qtyElem.setAttribute('type', 'number');
   qtyElem.setAttribute('id', 'qty');
   qtyElem.setAttribute('placeholder', 'Qty');
   qtyElem.setAttribute('name', 'qty');
   qtyElem.setAttribute('value', quantity.toString());
-  let unitElem = document.createElement('input');
+  const unitElem = document.createElement('input');
   unitElem.setAttribute('type', 'text');
   unitElem.setAttribute('id', 'unit');
   unitElem.setAttribute('placeholder', 'Unit');
   unitElem.setAttribute('name', 'unit');
   unitElem.setAttribute('value', unit);
-  let ingElem = document.createElement('input');
+  const ingElem = document.createElement('input');
   ingElem.setAttribute('type', 'text');
   ingElem.setAttribute('id', 'ingredient');
   ingElem.setAttribute('placeholder', 'Enter your ingredient');
   ingElem.setAttribute('name', 'ingredient');
   ingElem.setAttribute('value', ingredient);
   ingElem.classList.add('input__ingredient');
-  let deleteBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
   deleteBtn.setAttribute('type', 'button');
   deleteBtn.setAttribute('name', 'add');
   deleteBtn.classList.add('remove__ingredient');
   deleteBtn.textContent = "x";
+  deleteBtn.id = id;
+  deleteBtn.onclick = deleteRecipeItem;
 
   container.appendChild(qtyElem);
   container.appendChild(unitElem);
@@ -144,28 +125,27 @@ function makeIngredient(quantity, unit, ingredient, id) {
   return container;
 }
 
-//console.log(makeAdjustedIngredient(4, "grams", "sugar", 123));
-function makeAdjustedIngredient(quantity, unit, ingredient, id) {
-
-  let container = document.createElement('div');
+// generates an element for a adjusted ingredient
+const makeAdjustedIngredientElem = (quantity, unit, ingredient, id) => {
+  const container = document.createElement('div');
   container.setAttribute('id', "r" + id);
   container.classList.add('adjusted__ingredient');
 
-  let qtyElem = document.createElement('input');
+  const qtyElem = document.createElement('input');
   qtyElem.disabled = true;
   qtyElem.setAttribute('type', 'number');
   qtyElem.setAttribute('id', 'qty');
   qtyElem.setAttribute('placeholder', 'Qty');
   qtyElem.setAttribute('name', 'qty');
   qtyElem.setAttribute('value', quantity.toString());
-  let unitElem = document.createElement('input');
+  const unitElem = document.createElement('input');
   unitElem.disabled = true;
   unitElem.setAttribute('type', 'text');
   unitElem.setAttribute('id', 'unit');
   unitElem.setAttribute('placeholder', 'Unit');
   unitElem.setAttribute('name', 'unit');
   unitElem.setAttribute('value', unit);
-  let ingElem = document.createElement('input');
+  const ingElem = document.createElement('input');
   ingElem.disabled = true;
   ingElem.setAttribute('type', 'text');
   ingElem.setAttribute('id', 'ingredient');
@@ -177,5 +157,93 @@ function makeAdjustedIngredient(quantity, unit, ingredient, id) {
   container.appendChild(qtyElem);
   container.appendChild(unitElem);
   container.appendChild(ingElem);
+
   return container;
 }
+
+// Renders all components
+const render = () => {
+  if (recipeInfo.hasOwnProperty('conversionFactor')) {
+    // render conversion factor
+    document.querySelector('.yield__factor').textContent = recipeInfo.conversionFactor;
+
+    // clear ingredients
+    const recipeContainer = document.querySelector('.recipe__add');
+    const recipeFormElem = recipeContainer.children[0];
+    const formInputs = document.querySelectorAll('form input');
+    for (let i = 0; i < formInputs.length; i++) {
+      formInputs[i].value = '';
+    }
+    recipeContainer.innerHTML = '';
+    recipeContainer.appendChild(recipeFormElem);
+    const adjRecipeContainer = document.querySelector('.recipe__outcome');
+    adjRecipeContainer.innerHTML = '';
+
+    // render ingredients
+    recipeInfo.recipe.forEach(function (item) {
+      const recipeItemElem = makeIngredientElem(item.quantity, item.unit, item.ingredient, item.id);
+      recipeContainer.appendChild(recipeItemElem);
+
+      if (item.hasOwnProperty('adjustedQuantity')) {
+        const adjRecipeItemElem = makeAdjustedIngredientElem(item.adjustedQuantity, item.unit, item.ingredient, item.id);
+        adjRecipeContainer.appendChild(adjRecipeItemElem);
+      }
+    });
+  }
+}
+
+
+// Controller
+
+
+// Make conversiont factor based on initial yield
+const makeConversionFactorX = event => {
+  const initialYield = event.target.value;
+  const desiredYield = document.querySelector('#expected__yield').value;
+
+  createConversionFactor(initialYield, desiredYield);
+  createRecipeAdjustment();
+  render();
+}
+
+// Make conversiont factor based on desired yield
+const makeConversionFactorY = event => {
+  const initialYield = document.querySelector('#current__yield').value;
+  const desiredYield = event.target.value;
+
+  createConversionFactor(initialYield, desiredYield);
+  createRecipeAdjustment();
+  render();
+}
+
+// Add a recipe item
+const addRecipeItem = () => {
+  const quantity = document.querySelector('.recipe__add__form #qty').value;
+  const unit = document.querySelector('.recipe__add__form #unit').value;
+  const ingredient = document.querySelector('.recipe__add__form #ingredient').value;
+
+  createRecipeItem(quantity, unit, ingredient);
+  createRecipeAdjustment();
+  render();
+}
+
+// Delete a recipe item
+const deleteRecipeItem = event => {
+  const deleteButton = event.target;
+  const idToDelete = deleteButton.id;
+
+  removeRecipeItem(idToDelete);
+  render();
+}
+
+// initiate listening to yields and adding
+const beginControl = () => {
+  const initialYieldElem = document.querySelector('#current__yield');
+  const expectedYieldElem = document.querySelector('#expected__yield');
+  initialYieldElem.onchange = makeConversionFactorX;
+  expectedYieldElem.onchange = makeConversionFactorY;
+
+  const addBtn = document.querySelector('.add__ingredient');
+  addBtn.onclick = addRecipeItem;
+}
+beginControl();
